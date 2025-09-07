915b822d3851b4d210047fbf491e574909a2f80b
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-09-07 17:10:33 +0600

    added dynamic categories with api

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ callAllPlants = () => {
     });
 };
 showAllPlants = (plants) => {
+  mainContainer.innerHTML = "";
   plants.forEach((tree) => {
     mainContainer.innerHTML += `
       <div class="card bg-base-100 shadow-sm">
@@ -53,31 +54,61 @@ showAllPlants = (plants) => {
 };
 
 categoryContainer.addEventListener("click", (e) => {
-  const all_li = document.querySelectorAll("li");
+  if (e.target.localName !== "li") return;
+
+  const all_li = document.querySelectorAll("#cat-container li");
   all_li.forEach((li) => {
-    li.classList.remove("bg-[#15803D]", "text-white", "rounded-sm", "p-2");
+    li.classList.remove("bg-[#15803D]", "text-white", "rounded-sm", "px-[10px]", "py-[8px]");
   });
 
-  if (e.target.localName === "li") {
-    console.log(e.target.id);
-    e.target.classList.add("bg-[#15803D]", "text-white", "rounded-sm", "p-2");
-   
+  e.target.classList.add("bg-[#15803D]", "text-white", "rounded-sm", "px-[10px]", "py-[8px]");
+
+  if (e.target.id === "all-plants") {
+    callAllPlants(); 
+  } else {
     loadNewsByCategory(e.target.id);
   }
 });
+
 
 loadNewsByCategory = (id) => {
   fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.data.plants);
+      console.log(data.plants);
+      displayPlants(data.plants);
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
+const displayPlants = (plants) => {
+  mainContainer.innerHTML = "";
+  plants.forEach((tree) => {
+    mainContainer.innerHTML += `
+      <div class="card bg-base-100 shadow-sm">
+        <figure class="h-48 w-full">
+          <img src="${tree.image}" alt="${
+      tree.name
+    }" class="w-full h-full object-cover rounded-t-xl" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">${tree.name || "Unknown Tree"}</h2>
+          <p>${tree.description}</p>
+          <div class="card-actions justify-start">
+            <div class="badge badge-outline">${tree.category}</div>
+          </div>
+          <button class="btn bg-[#15803D] text-white rounded-full">Add to Cart</button>
+        </div>
+      </div>`;
+  });
+};
 
+document.getElementById("all-plants").addEventListener("click", () => {
+  callAllPlants();
+  console.log("fuck")
+});
 
 loadCategory();
 callAllPlants();
